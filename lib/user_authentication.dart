2d71cb26a71c3607394903af24c3d667b7ec7c41
HusseinReda97pro <pro.hussein.reda@gmail.com>
2022-11-13 04:17:44 +0200

    edit user response

--- lib/user_authentication.dart
+++ lib/user_authentication.dart
@@ -11,3 +11,4 @@ export 'src/repository/auth_repository.dart';
 export 'src/views/widgets/signin_facebook_button.dart';
 export 'src/views/widgets/signin_google_button.dart';
 export 'src/views/widgets/logout_button.dart';
+export 'src/views/widgets/signin_email_button.dart';
