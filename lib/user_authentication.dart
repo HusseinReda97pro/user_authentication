4912b1d4e979a09eca5ed644f1efba16d5b8995b
Pro.Hussein.Reda <hreda@arabdt.com>
2022-12-05 16:40:10 +0200

    validatie email

--- lib/user_authentication.dart
+++ lib/user_authentication.dart
@@ -13,5 +13,6 @@ export 'src/views/widgets/signin_google_button.dart';
 export 'src/views/widgets/logout_button.dart';
 export 'src/views/widgets/signin_email_button.dart';
 export 'src/views/widgets/signin_email_otp_button.dart';
+export 'src/views/widgets/signin_email_verify_otp_button.dart';
 
 export 'src/helper/validation.dart';
